08f70301cbb0e01b27252d3afb1408fc67567058
Yonni123 <86354472+Yonni123@users.noreply.github.com>
2024-01-15 01:17:51 +0100

    Added ObfNet source code

---
+++ Code/ObfNet/main.py
@@ -0,0 +1,109 @@
+import cv2
+from keras.utils import load_img
+from matplotlib import pyplot as plt
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential, load_model, Model
+from tensorflow.keras.layers import Dense, Dropout, Flatten, Reshape, Conv2D, MaxPooling2D
+from tensorflow.keras import backend as K
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ModelCheckpoint
+
+import tensorflow as tf
+import numpy as np
+tf.random.set_seed(1)   # For reproducibility
+np.random.seed(1)
+
+def get_inference(input_shape, num_classes):
+    model = Sequential()
+    model.add(Flatten(input_shape=input_shape))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(512, activation='relu'))
+    model.add(Dense(num_classes, activation='softmax'))
+    return model
+
+def get_obfmodel(input_shape, num_neuron=100):
+    model = Sequential()
+    model.add(Flatten(input_shape=input_shape))
+    model.add(Dense(num_neuron, activation='relu'))
+    model.add(Dense(28 * 28, activation='relu'))
+    model.add(Reshape(input_shape))
+    return model
+
+def predict_image(path, model):
+    size = (model.input_shape[1], model.input_shape[2])
+    img = load_img(path, target_size=size)
+    x = np.array(img)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+    x = np.expand_dims(x, axis=0)
+    x = np.expand_dims(x, axis=3)
+    x = x.astype('float32')
+    x /= 255
+    predictions = model.predict(x)
+    return np.argmax(predictions[0])
+
+def extract_obfnet(model, split_layer_index):
+    # Split the combined model into inference and obfuscation models
+    layers = model.layers[:split_layer_index]
+
+    # Extract input and output tensors
+    input_tensor = layers[0].input
+    output_tensor = layers[-1].output
+
+    # Create the obfuscation model
+    obfnet = Model(inputs=input_tensor, outputs=output_tensor)
+
+    # Transfer weights of the layers
+    for i in range(len(obfnet.layers)):
+        obfnet.layers[i].set_weights(model.layers[i].get_weights())
+
+    return obfnet
+
+def extract_infnet(model, split_layer_index):
+    # Split the combined model into inference and obfuscation models
+    layers = model.layers[split_layer_index:]
+
+    # Extract input and output tensors
+    input_tensor = layers[0].input
+    output_tensor = layers[-1].output
+
+    # Create the inference model
+    infnet = Model(inputs=input_tensor, outputs=output_tensor)
+
+    # Transfer weights of the layers
+    infnet.set_weights("models/mnist/inf-cnn.h5")
+
+    return infnet
+
+def obfuscate_image(path, model):
+    size = (model.input_shape[1], model.input_shape[2])
+    img = load_img(path, target_size=size)
+    x = np.array(img)
+    x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
+    x = np.expand_dims(x, axis=0)
+    x = np.expand_dims(x, axis=3)
+    x = x.astype('float32')
+    x /= 255
+    obf_image = model.predict(x)[0]
+    obf_image = np.squeeze(obf_image, axis=2)
+    obf_image *= 255
+    obf_image = obf_image.astype('uint8')
+    return obf_image
+
+
+num_classes = 10
+input_shape = (28, 28, 1)
+
+inference_model = get_inference(input_shape, num_classes)
+obfmodel = get_obfmodel(input_shape, 100)
+combined_model = Model(inputs=obfmodel.input, outputs=inference_model(obfmodel.output))
+combined_model.load_weights('models/mnist/combined-model.h5')
+
+img_path = '../Random_Images/MNIST/0.jpg'
+print("Prediction: ", predict_image(img_path, inference_model))
+
+obfnet = extract_obfnet(combined_model, 5)
+obf_image = obfuscate_image(img_path, obfnet)
+plt.imshow(obf_image, cmap='gray')
+plt.title('Obfuscated Image')
+plt.show()
